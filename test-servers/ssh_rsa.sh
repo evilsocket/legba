bb07e0586af495210cef75fe29d198bf73ff8c19
evilsocket <evilsocket@gmail.com>
2025-06-21 13:41:55 +0200

    misc: small fix or general refactoring i did not bother commenting

--- test-servers/ssh_rsa.sh
+++ test-servers/ssh_rsa.sh
@@ -1,13 +1,7 @@
+docker build -t ssh_rsa -f ssh_rsa.Dockerfile . && \
+clear &&
 docker run \
-  --name=openssh-server \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ=Etc/UTC \
-  -e SUDO_ACCESS=true \
-  -e PASSWORD_ACCESS=false \
-  -e USER_PASSWORD=test12345 \
-  -e USER_NAME=admin666 \
-  -v $(pwd)/ssh_rsa_config:/etc/ssh/sshd_config \
-  -p 2222:2222 \
+  -p 2222:22 \
+  -v $(pwd)/ssh_rsa_client_key.pub:/root/.ssh/authorized_keys \
   --rm \
-  lscr.io/linuxserver/openssh-server:latest
+  ssh_rsa
