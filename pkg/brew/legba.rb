c4c6f38399c026325cf9e87e3cd76264930f3564
evilsocket <evilsocket@gmail.com>
2025-09-12 19:14:57 +0200

    pkg: bumped brew formula

--- pkg/brew/legba.rb
+++ pkg/brew/legba.rb
@@ -1,15 +1,15 @@
 
 class Legba < Formula
-    version '1.1.1'
+    version '1.2.0'
     desc "Legba is a multiprotocol credentials bruteforcer / password sprayer and enumerator."
     homepage "https://github.com/evilsocket/legba"
   
     if OS.mac?
         url "https://github.com/evilsocket/legba/releases/download/#{version}/legba-#{version}-apple-darwin-arm64.tar.gz"
-        sha256 "f8df9c40ae853113a0bab81e80486d221881d01b0014ed0aa72fe72c7b75e52f"
+        sha256 "5faf2f734b64e6128fd4929b694f8ec60b6be6a55f753d424bcaf1706e26cf93"
     elsif OS.linux?
         url "https://github.com/evilsocket/legba/releases/download/#{version}/legba-#{version}-linux-x86_64.tar.gz"
-        sha256 "7a01c975e5329e8f819ca552b9b734102ffbd30f1b6f7739b1cf1108a22eb096"
+        sha256 "0c286d64edec605f74b71d96eae1d8605ec4c1770a62304e93eb15cfce9c3455"
     end
   
     conflicts_with "legba"
